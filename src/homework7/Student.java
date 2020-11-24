df83cae040aec85811c03ff971afa20e7da79536
nagi5991@gmail.com <135764Sap>
2020-11-25 02:20:28 +0400

    Homework 7 - Initial Commit

---
+++ src/homework7/Student.java
@@ -0,0 +1,69 @@
+package homework7;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int year;
+    private char gender;
+    private double mark;
+
+    public Student(String firstName, String lastName, int year, char gender, double mark) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+        this.gender = gender;
+        this.mark = mark;
+    }
+
+    public void printFullName(){
+        System.out.println("The full name is: " + firstName + " " + lastName);
+    }
+
+    public void printStudentInfo(){
+        printFullName();
+        System.out.println("The student was born in " + year);
+        System.out.println("The student is " + (gender=='m'?"male":"female"));
+        System.out.println("The student mark is " + mark);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+}
